1f34f5d75e1c974c4f80cf6cfcc5b14cc96e9e54
Ankit Tyagi <ankit.tyagi555@gmail.com>
2025-03-10 16:26:20 +0530

    add:manifest.json file

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://cryptofearandgreedindex.com'
+  const baseUrl = 'https://www.cryptofearandgreedindex.com'
   
   return [
     {
