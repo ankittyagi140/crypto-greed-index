8b116122bf07812395adc56d3cd229660ea754e7
Ankit Tyagi <ankit.tyagi555@gmail.com>
2025-03-10 16:31:23 +0530

    add:manifest.json file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/admin/'],
     },
-    sitemap: 'https://cryptofearandgreedindex.com/sitemap.xml',
+    sitemap: 'https://www.cryptogreedindex.com/sitemap.xml',
   }
 } 
