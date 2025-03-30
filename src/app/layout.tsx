ebcee8c03a98e13c26943dcdf78f582fdcb9a46e
Ankit Tyagi <aki.tyagi1991@gmail.com>
2025-03-30 19:07:25 +0530

    add:adsense data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -275,6 +275,8 @@ export default function RootLayout({
             })
           }}
         />
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1332831285527693"
+          crossOrigin="anonymous"></Script>
       </head>
       <body className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
         <Providers>
